0656f3dac87b21261e1e08bca68939eebc8698ba
KaeTheDev <kaethedev@gmail.com>
2026-01-07 15:31:30 -0500

    Defined the port the server will run on

--- src/server.ts
+++ src/server.ts
@@ -3,3 +3,6 @@ import express, { Request, Response } from 'express';
 
 // 2. Create an instance of an Express application
 const app = express();
+
+// 3. Define the port the server will run on
+const port = 3000;
